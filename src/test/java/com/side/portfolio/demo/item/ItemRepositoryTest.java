a2dd30f90543555dcc64992ee721034ef4f006f8
Knitty-Kim <kimkitae1208@gmail.com>
2023-12-04 23:30:46 +0900

    ItemRepositoryTest!

--- src/test/java/com/side/portfolio/demo/item/ItemRepositoryTest.java
+++ src/test/java/com/side/portfolio/demo/item/ItemRepositoryTest.java
@@ -1,10 +1,11 @@
 package com.side.portfolio.demo.item;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 
-import static org.junit.Assert.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class ItemRepositoryTest {
 
@@ -26,24 +27,76 @@ public class ItemRepositoryTest {
 
         //then
         Item foundItem = itemRepository.findById(item.getItemRid());
-        Assertions.assertThat(foundItem).isEqualTo(savedItem);
+        assertThat(foundItem).isEqualTo(savedItem);
 
 
     }
 
     @Test
     public void findById() {
+        //given
+        Item itemA = new Item("Fritos", 1, 10, "OK");
+        Item itemB = new Item("Lays", 2, 20, "OK");
+        itemRepository.save(itemA);
+        itemRepository.save(itemB);
+
+        //when
+        Item foundItemA = itemRepository.findById(itemA.getItemRid());
+
+        //then
+        assertThat(foundItemA.getItemRid()).isEqualTo(itemA.getItemRid());
+        assertThat(foundItemA.getItemRid()).isNotEqualTo(itemB.getItemRid());
+
     }
 
     @Test
     public void findAll() {
+        //given
+        Item itemA = new Item("Fritos", 1, 10, "OK");
+        Item itemB = new Item("Lays", 2, 20, "OK");
+        itemRepository.save(itemA);
+        itemRepository.save(itemB);
+
+        //when
+        List<Item> itemList = itemRepository.findAll();
+
+        //then
+        assertThat(itemList.size()).isEqualTo(2);
+        assertThat(itemList).contains(itemA, itemB);
     }
 
     @Test
     public void update() {
+        //given
+        Item itemA = new Item("Fritos", 1, 10, "OK");
+        Item itemB = new Item("ChipsAhoy", 3, 30, "OK");
+        itemRepository.save(itemA);
+
+        //when
+        itemRepository.update(itemA.getItemRid(), itemB);
+
+        //then
+        assertThat(itemA.getItemName()).isEqualTo(itemB.getItemName());
+        assertThat(itemA.getPrice()).isEqualTo(itemB.getPrice());
+        assertThat(itemA.getQuantity()).isEqualTo(itemB.getQuantity());
+        assertThat(itemA.getStatus()).isEqualTo(itemB.getStatus());
     }
 
     @Test
     public void clear() {
+        //given
+        Item itemA = new Item("Fritos", 1, 10, "OK");
+        Item itemB = new Item("Lays", 2, 20, "OK");
+        itemRepository.save(itemA);
+        itemRepository.save(itemB);
+
+        //when
+        itemRepository.clear();
+
+        //then
+        List<Item> itemList = itemRepository.findAll();
+        assertThat(itemList.size()).isEqualTo(0);
+        assertThat(itemList).isEmpty();
+
     }
 }
